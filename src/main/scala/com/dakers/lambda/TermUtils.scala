a694c8689435fa242de0d69372a4e20243d73aa1
Devon Akers <devon.p.akers@vanderbilt.edu>
2019-05-04 01:40:14 -0400

    more composition

--- src/main/scala/com/dakers/lambda/TermUtils.scala
+++ src/main/scala/com/dakers/lambda/TermUtils.scala
@@ -4,9 +4,11 @@ object TermUtils {
 
   def print(t : Term) : String = t match {
     case Var(v) => v
-    case App(v1, v2) => print(v1) + print(v2)
-    case Arrow(v1, v2) => print(v1) + " -> " + print(v2)
+    case App(v1, v2) => paren(print(v1) + print(v2))
+    case Arrow(v1, v2) => paren(print(v1) + " -> " + print(v2))
   }
+
+  def paren(s : String) = "(" + s + ")"
 
   def main(args: Array[String]): Unit = {
     println("Testing 'print()' method for lambda terms.")
@@ -14,5 +16,7 @@ object TermUtils {
     println("A application of a variable M to another variable N is printed as '" + print(App(Var("M"), Var("N"))) +"'")
     println("An arrow type from a variable M to another variable N is printed as '" + print(Arrow(Var("M"), Var("N"))) +"'")
     println("An application of M to M -> N  is printed as '" + print(Arrow(Var("M"), Arrow(Var("M"), Var("N")))) +"'")
+    println("An application of M to M -> N  is printed as '" + print(App(Var("M"), Arrow(Var("M"), Var("N")))) +"'")
+    println("An arrow from P to application of M to M -> N  is printed as '" + print(Arrow(Var("P"), App(Var("M"), Arrow(Var("M"), Var("N"))))) +"'")
   }
 }
