dd4b6660b44c2b6a0d8b18add0295002ad8da8ec
Devon Akers <devon.p.akers@vanderbilt.edu>
2019-05-04 01:07:29 -0400

    Hello World

---
+++ build.sbt
@@ -0,0 +1,5 @@
+name := "proof-assistant"
+
+version := "0.1"
+
+scalaVersion := "2.12.8"
---
+++ src/main/scala/com/dakers/Main.scala
@@ -0,0 +1,7 @@
+package com.dakers
+
+object Main {
+    def main(args: Array[String]) {
+      println("Hello, world!")
+    }
+}
