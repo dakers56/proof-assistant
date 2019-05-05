c78067f189bc5f4a328a6c12acf40d71c8deff7a
Devon Akers <devon.p.akers@vanderbilt.edu>
2019-05-05 13:02:40 -0400

    testing toString for var, app and abst terms

--- src/main/scala/com/dakers/lambda/Term.scala
+++ src/main/scala/com/dakers/lambda/Term.scala
@@ -1,9 +1,35 @@
 package com.dakers.lambda
 
+/** Represents the grammar
+ * Λ = V |(ΛΛ)|(λV . Λ)
+ *
+ * Nederpelt, Rob. Type Theory and Formal Proof: An Introduction (Kindle Locations 645-646). Cambridge University Press. Kindle Edition.
+ */
 abstract class Term
 
-case class Var(varName: String) extends Term
+object Term {
+  val abstOp = "/|"
+  val abstSep = "."
+}
 
-case class App(v1: Term, v2: Term) extends Term
+abstract class Var(val varName: String) extends Term{
+  override def toString: String = varName
+}
 
-case class Arrow(v1: Term, v2: Term) extends Term
+object Var {
+  def unapply(arg: Var): Option[String] = Some(arg.varName)
+}
+
+case class App(v1: Term, v2: Term) extends Term {
+  override def toString: String = v1.toString + v2.toString
+}
+
+case class Abst(v1: Term, v2: BVar) extends Term {
+  override def toString: String = Term.abstOp + v2.toString + Term.abstSep + v1.toString
+}
+
+case class BVar(bVarName: String) extends Var(bVarName)
+
+case class FVar(fVarName: String) extends Var(fVarName)
+
+
--- src/main/scala/com/dakers/lambda/TermUtils.scala
+++ src/main/scala/com/dakers/lambda/TermUtils.scala
@@ -1,22 +1,12 @@
 package com.dakers.lambda
 
 object TermUtils {
-
-  def print(t : Term) : String = t match {
-    case Var(v) => v
-    case App(v1, v2) => paren(print(v1) + print(v2))
-    case Arrow(v1, v2) => paren(print(v1) + " -> " + print(v2))
-  }
-
-  def paren(s : String) = "(" + s + ")"
-
   def main(args: Array[String]): Unit = {
-    println("Testing 'print()' method for lambda terms.")
-    println("A variable M is printed as '" + print(Var("M")) +"'")
-    println("A application of a variable M to another variable N is printed as '" + print(App(Var("M"), Var("N"))) +"'")
-    println("An arrow type from a variable M to another variable N is printed as '" + print(Arrow(Var("M"), Var("N"))) +"'")
-    println("An application of M to M -> N  is printed as '" + print(Arrow(Var("M"), Arrow(Var("M"), Var("N")))) +"'")
-    println("An application of M to M -> N  is printed as '" + print(App(Var("M"), Arrow(Var("M"), Var("N")))) +"'")
-    println("An arrow from P to application of M to M -> N  is printed as '" + print(Arrow(Var("P"), App(Var("M"), Arrow(Var("M"), Var("N"))))) +"'")
+    println("Testing 'toString()' method for lambda terms.")
+    println("A variable M is printed as '" + BVar("M") + "'")
+    println("A application of a variable M to another variable N is printed as '" + App(BVar("M"), BVar("N")) + "'")
+    println("An abstraction of M via N ais printed as '" + Abst(FVar("M"),BVar("N")))
+    println("An abstraction of MN via N is printed as '" + Abst(App(FVar("M"), BVar("N")), BVar("N")) + "'")
+    println("-----------------------")
   }
 }
