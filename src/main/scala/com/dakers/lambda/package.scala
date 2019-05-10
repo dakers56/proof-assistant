cb4bec0e94debf8f298ff262bcd115524d6840d3
Devon Akers <devon.p.akers@vanderbilt.edu>
2019-05-09 22:06:44 -0400

    testing deserialization

--- src/main/scala/com/dakers/lambda/package.scala
+++ src/main/scala/com/dakers/lambda/package.scala
@@ -3,48 +3,42 @@ package com.dakers
 package object lambda {
 
   val AbstOp = raw"/|"
-  val AbstSep = raw"."
-  val AbstRegex = s"^$AbstOp(.*)".r
-  val VarRegex = raw"(.)".r //For now, only handling 1-character variables
-  val AppRegex = raw"(.{2,})".r
+  val AbstSep = "."
+  val AbstRegex = raw"/\|(.)\.(.*)".r
+  val VarRegex = "(.)".r
+  val AppLRegex = raw"\((\(.*\))(.)\)".r
+  val AppRRegex = raw"\((.)(\(.*\))\)".r
+  val App0Regex = raw"\((.)(.)\)".r
+//  val AppAbstR = raw"(.*)(?=\/\|)(.*)(?=\))".r
+  val AppAbstR = raw"\((.*)/\|(.)\.(.*)\)".r
 
 
   def lam(s: String): Term = {
-    if (s.length == 1) return Var(s)
-    if (s.startsWith(AbstOp)) {
-      val v = splitAbst(s)
-      Abst(lam(v._2), Var(v._1))
-    }
-    else {
-      val v = splitApp(s)
-      if (v._2.length == 1) {
-        App(Var(v._1), lam(v._2))
+
+    s match {
+      case AbstRegex(x, t) => {
+        Abst(lam(t), Var(x))
       }
-      else {
-        val x = v._1
-        val y = v._2.charAt(0).toString
-        val xy = App(Var(x), Var(y))
-        var xyz = xy
-
-        for (i <- 1 until v._2.length) {
-          xyz = App(xyz, Var(v._2.charAt(i).toString))
-        }
-        xyz
+      case AppLRegex(l, r) => {
+        App(lam(l), lam(r))
+      }
+      case AppRRegex(l, r) => {
+        App(lam(l), lam(r))
+      }
+      case App0Regex(l, r) => {
+        App(Var(l), Var(r))
+      }
+      case VarRegex(x) => {
+        Var(x)
+      }
+      case AppAbstR(l,x, r) => {
+        App(lam(l), Abst(lam(r), Var(x)))
+      }
+      case _ => {
+        throw new RuntimeException(s"$s was not a well formed lambda expression. Make sure that it has () around all application terms.")
       }
     }
   }
-
-
-  def splitApp(s: String) = {
-    (s.substring(0, 1), s.substring(1))
-  }
-
-  def splitAbst(s: String): (String, String) = {
-    val i = s.indexOf(AbstSep)
-    (s.substring(0, i).replace(AbstOp, ""), s.substring(i + 1))
-  }
-
-  def varName(binderStr: String) = binderStr.replaceFirst(AbstOp, "")
 
 
 }
--- src/test/scala/com/dakers/lambda/PackageTest.scala
+++ src/test/scala/com/dakers/lambda/PackageTest.scala
@@ -17,15 +17,27 @@ class PackageTest extends FlatSpec with Matchers {
   }
 
   s"$AbstOp x $AbstSep $AbstOp y $AbstSep xy" should "deserialize to Abst(App(Var(x), Var(y)),Var(x))" in {
-    lam(s"/|x./|y.xy") should be(Abst(Abst(App(Var("x"), Var("y")), Var("y")), Var("x")))
+    lam(s"/|x./|y.(xy)") should be(Abst(Abst(App(Var("x"), Var("y")), Var("y")), Var("x")))
   }
 
   s"$AbstOp z $AbstSep $AbstOp x $AbstSep $AbstOp y $AbstSep xyz" should "deserialize to Abst(App(Var(x), Var(y)),Var(x))" in {
-    lam(s"/|z./|x./|y.xyz") should be(Abst(Abst(Abst(App(App(Var("x"), Var("y")), Var("z")), Var("y")), Var("x")), Var("z")))
+    lam(s"/|z./|x./|y.((xy)z)") should be(Abst(Abst(Abst(App(App(Var("x"), Var("y")), Var("z")), Var("y")), Var("x")), Var("z")))
   }
 
   s"$AbstOp u $AbstSep $AbstOp z $AbstSep $AbstOp x $AbstSep $AbstOp y $AbstSep xyz" should "deserialize to Abst(Abst(Abst(Abst(App(App(App(Var(\"x\"), Var(\"y\")), Var(\"z\")), Var(\"u\")), Var(\"y\")), Var(\"x\")), Var(\"z\")), Var(\"u\"))" in {
-    lam(s"/|u./|z./|x./|y.xyzu") should be(Abst(Abst(Abst(Abst(App(App(App(Var("x"), Var("y")), Var("z")), Var("u")), Var("y")), Var("x")), Var("z")), Var("u")))
+    lam(s"/|u./|z./|x./|y.(((xy)z)u)") should be(Abst(Abst(Abst(Abst(App(App(App(Var("x"), Var("y")), Var("z")), Var("u")), Var("y")), Var("x")), Var("z")), Var("u")))
+  }
+
+  s"$AbstOp w $AbstOp u $AbstSep $AbstOp z $AbstSep $AbstOp x $AbstSep $AbstOp y $AbstSep xyzu $AbstOp t $AbstSep t" should "deserialize to Abst(Abst(Abst(Abst(App(App(App(Var(\"x\"), Var(\"y\")), Var(\"z\")), Var(\"u\")), Var(\"y\")), Var(\"x\")), Var(\"z\")), Var(\"u\"))" in {
+    lam(s"/|w./|u./|z./|x./|y.((((xy)z)u)w)") should be(
+      Abst(Abst(Abst(Abst(Abst(App(App(App(App(Var("x"), Var("y")), Var("z")), Var("u")), Var("w")), Var("y")), Var("x")), Var("z")), Var("u")), Var("w")))
+  }
+
+  s"$AbstOp w $AbstOp u $AbstSep $AbstOp z $AbstSep $AbstOp x $AbstSep $AbstOp y $AbstSep xyzu" should "deserialize to Abst(Abst(Abst(Abst(App(App(App(Var(\"x\"), Var(\"y\")), Var(\"z\")), Var(\"u\")), Var(\"y\")), Var(\"x\")), Var(\"z\")), Var(\"u\"))" in {
+    val xyzuw = App(App(App(App(Var("x"), Var("y")),Var("z")),Var("u")),Var("w"))
+    val xyzuw_t = App(xyzuw,Abst(Var("t"), Var("t")))
+
+    lam(s"/|w./|u./|z./|x./|y.(((((xy)z)u)w)/|t.t)") should be(Abst(Abst(Abst(Abst(Abst(xyzuw_t,Var("y")),Var("x")),Var("z")),Var("u")),Var("w")))
   }
 
 }
